ad2bf3960c0d7e5bc12f22c1e0ce23798955bc5d
LuciusEverest <61430189+LuciusEverest@users.noreply.github.com>
2020-07-01 14:51:51 -0600

    update

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -9,26 +9,46 @@
 
 const size_t NUM_POINTS = 40;
 std::vector<bleh::Vector2> points;
+bleh::Vector2 position{ 400.0f, 300.0f };
+float speed = 5.0f;
 
 bool Update(float dt) 
 {
-	for (size_t i = 0; i < NUM_POINTS; i++)
+	bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
+
+	int x, y;
+	Core::Input::GetMousePos(x, y);
+
+	bleh::Vector2 target = bleh::Vector2{ x, y };
+	bleh::Vector2 direction = target - position; //head - tail <-
+
+	direction.Normalize();
+
+	//if (direction.Length() < 50.0f)
 	{
-		points[i] = bleh::Vector2{ bleh::random(0.0f, 800.0f), bleh::random(0.0f, 600.0f) };
+		position = position + (-direction * speed);
 	}
 
-	return false; 
+	//if (Core::Input::IsPressed('A')) position += bleh::Vector2{ -1.0f, 0.0f } * speed;
+	//if (Core::Input::IsPressed('D')) position += bleh::Vector2{ 1.0f, 0.0f } * speed;
+
+	for (bleh::Vector2& point : points)
+	{
+		point = bleh::Vector2{ bleh::random(-10.0f, 10.0f), bleh::random(-10.0f, 10.0f) };
+	}
+
+	return quit; 
 }
 
 void Draw(Core::Graphics& graphics) 
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
-	graphics.DrawLine(bleh::random(0.0f, 800.0f), bleh::random(0.0f, 600.0f), bleh::random(0.0f, 800.0f), bleh::random(0.0f, 600.0f));
+	//graphics.DrawLine(bleh::random(0.0f, 800.0f), bleh::random(0.0f, 600.0f), bleh::random(0.0f, 800.0f), bleh::random(0.0f, 600.0f));
 
 	for (size_t i = 0; i < NUM_POINTS - 1; i +=2)
 	{
-		bleh::Vector2 p1 = points[i];
-		bleh::Vector2 p2 = points[i + 1];
+		bleh::Vector2 p1 = position + points[i] * 4.0f;
+		bleh::Vector2 p2 = position + points[i + 1] * 4.0f;
 
 		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
 	}
